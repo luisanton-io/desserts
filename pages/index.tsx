179c64b221e90a2a4122082a36da4f77d0d105d8
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-05-03 16:08:38 +0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import DessertHeader from '../components/Main/Header'
 import Main from '../components/Main'
 import DessertCtx from '../context/dessertCtx'
 import { Dessert } from '../interfaces'
